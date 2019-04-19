c5e841831aaab11bb385d07a6eada11fd7c18ec5
YcodeMan <1130578809@qq.com>
2019-04-19 20:05:58 +0800

    add config

--- webpack.config.js
+++ webpack.config.js
@@ -4,5 +4,24 @@ module.exports = {
 	output: {
 		path: __dirname + "/public",
 		filename: "bundle.js"
+	},
+	devServer: {
+		contentBase: "./public",
+		historyApiFallback: true,
+		inline: true
+	},
+	module: {
+		rules: [
+			{
+				test: /\.css$/,
+				use: [
+					{	
+						loader: "style-loader"
+					}, {
+						loader: "css-loader"
+					}
+				]
+			}
+		]
 	}
 }
