2c1b1ca9f6722e0b8d586c453ff559e47665d8b5
YcodeMan <1130578809@qq.com>
2019-04-20 14:58:31 +0800

    add setContent fn

--- app/dom/dom.js
+++ app/dom/dom.js
@@ -1,28 +1,101 @@
-module.exports = Element.prototype.addClass
-			   = HTMLDocument.prototype.addClass = addClass;
-			   Element.prototype.removeClass 
-			   = HTMLDocument.prototype.removeClass = removeClass;
 
-function addClass(cName) {
+Object.defineProperties(Element.prototype, {
+			addClass: {
+				value: addClass,
+				writable: false,
+				configurable: true,
+				enumerable: false
+
+			},
+			removerClass: {
+				value: removeClass,
+				writable: false,
+				configurable: true,
+				enumerable: false
+			},
+			css: {
+				value: css,
+				writable: false,
+				configurable: true,
+				enumerable: false
+			}
+		}
+)
+
+function addClass(className) {
 	var len = this.length || 0;
 	if (len > 0) {
 		for (var i = 0; i < len; i++) {
-			this[i] = this[i].addClass(cName);
+			this[i] = this[i].addClass(className);
 		}
 	} else {
-		this.className = this.className + ' ' + cName;
+		this.className = this.className + ' ' + className;
 	}
 	return this;
 }
-function removeClass(cName) {
+function removeClass(className) {
 	var len = this.length || 0;
 	if (len > 0) {
 		for (var i = 0; i < len; i++) {
 			this[i] = this[i].className = this[i].className.replace( 
-					new RegExp("(\\s|^)" + cName + "(\\s|$)" ), "");	
+					new RegExp("(\\s|^)" + className + "(\\s|$)" ), "");	
 		}
 	}
 	this.className = this.className.replace( 
-					new RegExp("(\\s|^)" + cName + "(\\s|$)" ), "");
+					new RegExp("(\\s|^)" + className + "(\\s|$)" ), "");
 	return this;					
+}
+function css(attr, value) {
+	var len = this.length || 0;
+	if (len > 0) {
+		for(var i = 0; i < len; i++) {
+			this[i] = this[i].css(attr, value);
+		}
+	} else {
+		if (type(attr) === 'object') {
+			for (var k in attr) {
+				this.style[k] = attr[k];
+			}
+		} else if (typeof attr === 'string' && !value) {
+			return  removeCssUnit(getComputedStyle(this, null)[attr]);
+		} else if (typeof attr === 'string' && value) {
+			this.style[attr] = value;
+		}
+	}
+	return this;
+}
+function removeCssUnit(value) {
+	var reg = /px|em|rem|vh|vw|ex$/;
+	if (reg.test(value)) {
+		return parseInt(value);
+	}
+	return value;
+}
+
+
+Object.defineProperty(Object.prototype, 'type',{
+	value: type,
+	writable: true,
+	configurable: true,
+	enumerable: false
+});
+var objType = {
+	'[object Boolean]': 'boolean',
+	'[object Number]': 'number',
+	'[object Function]': 'function',
+	'[object Array]': 'array',
+	'[object RegExp]': 'regexp',
+	'[object String]': 'string',
+	'[object Date]': 'date',
+	'[object Error]': 'error',
+	'[object Symbol]': 'symbol',
+	'[object Object': 'object'
+};
+var objToStr = Object.prototype.toString;
+function type(obj) {
+	if (obj == null) {
+		return obj + '';
+	}
+	return typeof obj === 'object' || typeof obj === 'function' 
+	? objType[objToStr.call(obj)] || 'object' : typeof obj;
 }
--- app/main.js
+++ app/main.js
@@ -1,6 +1,6 @@
 const accordion = require('./accordion/accordion.js');
 var css =	require('./accordion/accordion.css');
-console.log(css);
+
 var script = document.createElement('script');
 script.innerHTML = 'accordion()';
 document.body.appendChild(script);
